2694adb3d962167ccf4cb9b259c77fde8ae754b5
ManiMuthu <86956699+mani195102@users.noreply.github.com>
2024-05-31 04:01:43 +0530

    Delete Hall-booking-app directory

--- Hall-booking-app/routes/bookings.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const bookingController = require('../controllers/bookingController');
-
-// Book a room
-router.post('/', bookingController.bookRoom);
-
-module.exports = router;
